2987ae299a3d1ab688befeb6cf10ccab3ebe0fdb
Andrew McWilliams <andy@jahya.net>
2015-06-27 18:21:39 -0400

    Adds empty setup method

---
+++ src/ofxTemplate.cpp
@@ -0,0 +1,5 @@
+#include "ofxTemplate.h"
+
+void ofxTemplate::setup() {
+    
+}
---
+++ src/ofxTemplate.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofxTemplate : public ofBaseApp{
+
+public:
+    void setup();
+};
