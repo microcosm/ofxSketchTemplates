66950dbabdef1a9c4e940a173a09644013fa1501
Andrew McWilliams <andy@jahya.net>
2015-06-27 18:45:51 -0400

    Copies code from daily sketches gif encoder template

--- src/ofxTemplate.cpp
+++ src/ofxTemplate.cpp
@@ -1,5 +1,77 @@
 #include "ofxTemplate.h"
 
-void ofxTemplate::setup() {
+void ofxTemplate::setup(string _filename, int _width, int _height, bool _paused, bool _slowMode) {
+    slowMode = _slowMode;
+    paused = _paused;
+    width = _width;
+    height = _height;
+    frameDuration = 0.2;
+    colors = 256;
+    saveOnFrame = -1;
+
+    filename = _filename + ".gif";
+    framerate = slowMode ? 5 : 10;
+
+    halfWidth = width * 0.5;
+    halfHeight = height * 0.5;
+    doubleWidth = width * 2;
+    doubleHeight = height * 2;
+    tripleWidth = width * 3;
+    tripleHeight = height * 3;
+
+    ofSetFrameRate(framerate);
+    gifEncoder.setup(width, height, frameDuration, colors);
+    renderingNow = false;
+    renderMessage = "";
     
+    fbo.allocate(width, height, GL_RGB);
+    fbo.begin();
+    ofClear(255, 255, 255, 0);
+    fbo.end();
+}
+
+void ofxTemplate::begin(){
+    fbo.begin();
+}
+
+void ofxTemplate::end(){
+    fbo.end();
+}
+
+void ofxTemplate::draw(){
+    ofSetColor(ofColor::white);
+    fbo.draw(0, 0);
+    ofDrawBitmapString(
+       "Recording to frame #" +
+       ofToString(saveOnFrame) +
+       " at " +
+       ofToString(ofGetFrameRate()) +
+       "fps...\nCurrent frame: " +
+       ofToString(ofGetFrameNum()) +
+       "\n" + renderMessage,
+       20, height - 50);
+}
+
+void ofxTemplate::captureFrame(){
+    if(!renderingNow && !paused) {
+        fbo.readToPixels(pixels);
+        gifEncoder.addFrame(pixels.getPixels(), width, height,
+                            pixels.getBitsPerPixel(), frameDuration);
+        
+        if(ofGetFrameNum() == saveOnFrame) {
+            renderGif();
+        }
+    }
+}
+
+void ofxTemplate::renderGif() {
+    if(!renderingNow) {
+        renderingNow = true;
+        gifEncoder.save(filename);
+        renderMessage = "Now rendering";
+    }
+}
+
+void ofxTemplate::exit(){
+    gifEncoder.exit();
 }
--- src/ofxTemplate.h
+++ src/ofxTemplate.h
@@ -1,9 +1,30 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGifEncoder.h"
 
 class ofxTemplate : public ofBaseApp{
 
 public:
-    void setup();
+    void setup(string _filename, int _width=ofGetWidth(), int _height=ofGetHeight(), bool _paused=false, bool _slowMode=false);
+    void begin();
+    void end();
+    void draw();
+    void captureFrame();
+    void renderGif();
+    void exit();
+
+    int width, height;
+    int halfWidth, halfHeight;
+    int doubleWidth, doubleHeight;
+    int tripleWidth, tripleHeight;
+protected:
+    int colors, frameDuration;
+    int framerate, saveOnFrame;
+    string filename, renderMessage;
+    bool slowMode, renderingNow, paused;
+    
+    ofxGifEncoder gifEncoder;
+    ofFbo fbo;
+    ofPixels pixels;
 };
