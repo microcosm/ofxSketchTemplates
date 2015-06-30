0f89db314a54a34dfdc2ad14f4eeae5aace2e45d
Andrew McWilliams <andy@jahya.net>
2015-06-30 00:01:38 -0400

    Adds pausability

--- src/ofxGifEncoderTemplate.cpp
+++ src/ofxGifEncoderTemplate.cpp
@@ -1,8 +1,8 @@
 #include "ofxGifEncoderTemplate.h"
 
-void ofxGifEncoderTemplate::setup(string _filename, int _numFramesToRender, int _width, int _height) {
+void ofxGifEncoderTemplate::setup(string _filename, int _recordToFrameCount, int _width, int _height) {
     filename = _filename + ".gif";
-    renderOnFrame = _numFramesToRender - 1;
+    renderOnFrame = _recordToFrameCount;
     width = _width;
     height = _height;
     frameDuration = 0.2;
@@ -10,7 +10,8 @@ void ofxGifEncoderTemplate::setup(string _filename, int _numFramesToRender, int 
     slowMode = false;
     paused = false;
     renderingNow = false;
-    renderMessage = "";
+    renderMessage = "[[Recording...]]";
+    recordingMessage = "Recording to frame " + ofToString(_recordToFrameCount) + " frames";
 
     halfWidth = width * 0.5;
     halfHeight = height * 0.5;
@@ -27,6 +28,13 @@ void ofxGifEncoderTemplate::setup(string _filename, int _numFramesToRender, int 
     framerate = slowMode ? 5 : 10;
     ofSetFrameRate(framerate);
     gifEncoder.setup(width, height, frameDuration, colors);
+}
+
+void ofxGifEncoderTemplate::setupPaused(string _filename, int _recordFromFrameCount, int _recordToFrameCount, int _width, int _height) {
+    unpauseOnFrame = _recordFromFrameCount - 1;
+    setup(_filename, _recordToFrameCount, _width, _height);
+    recordingMessage = "Recording from frame " + ofToString(_recordFromFrameCount) + " to frame " + ofToString(_recordToFrameCount);
+    pause();
 }
 
 void ofxGifEncoderTemplate::begin(){
@@ -46,22 +54,21 @@ void ofxGifEncoderTemplate::draw(){
     ofSetColor(ofColor::white);
     fbo.draw(0, 0);
     ofDrawBitmapString(
-       "Recording to frame #" +
-       ofToString(renderOnFrame) +
-       " at " +
-       ofToString(ofGetFrameRate()) +
-       "fps...\nCurrent frame: " +
-       ofToString(ofGetFrameNum()) +
-       "\n" + renderMessage,
-       20, height - 50);
+       recordingMessage +
+       "\n\n" + renderMessage + "\n\n" +
+       "Frame count: " + ofToString(ofGetFrameNum() + 1) + " / " +
+       ofToString(ofGetFrameRate()) + "fps...",
+       20, height - 100);
 }
 
 void ofxGifEncoderTemplate::pause(){
     paused = true;
+    renderMessage = "[[Paused...]]";
 }
 
 void ofxGifEncoderTemplate::unpause(){
     paused = false;
+    renderMessage = "[[RECORDING...]]";
 }
 
 void ofxGifEncoderTemplate::togglePause(){
@@ -82,13 +89,17 @@ void ofxGifEncoderTemplate::captureFrame(){
             renderGif();
         }
     }
+
+    if(ofGetFrameNum() == unpauseOnFrame) {
+        unpause();
+    }
 }
 
 void ofxGifEncoderTemplate::renderGif() {
     if(!renderingNow) {
         renderingNow = true;
         gifEncoder.save(filename);
-        renderMessage = "Now rendering";
+        renderMessage = "[[Rendering...]]";
     }
 }
 
--- src/ofxGifEncoderTemplate.h
+++ src/ofxGifEncoderTemplate.h
@@ -6,7 +6,8 @@
 class ofxGifEncoderTemplate : public ofBaseApp{
 
 public:
-    void setup(string _filename, int _numFramesToRender=-1, int _width=ofGetWidth(), int _height=ofGetHeight());
+    void setup(string _filename, int _renderFromFrameCount=-1, int _width=ofGetWidth(), int _height=ofGetHeight());
+    void setupPaused(string _filename, int _unpauseFromFrameCount=-1, int _renderFromFrameCount=-1, int _width=ofGetWidth(), int _height=ofGetHeight());
     void begin();
     void end();
     void endAndCaptureFrame();
@@ -25,8 +26,8 @@ public:
     int tripleWidth, tripleHeight;
 protected:
     int colors, frameDuration;
-    int framerate, renderOnFrame;
-    string filename, renderMessage;
+    int framerate, renderOnFrame, unpauseOnFrame;
+    string filename, renderMessage, recordingMessage;
     bool slowMode, renderingNow, paused;
     
     ofxGifEncoder gifEncoder;
